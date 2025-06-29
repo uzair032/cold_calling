149b83f0916216a7e061c777beaca9dc1028a182
Uzair <137289494+uzair032@users.noreply.github.com>
2025-06-29 18:21:01 +0500

    Update UploadForm.js

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -16,7 +16,7 @@ const UploadForm = () => {
 
     setLoading(true);
     try {
-      const res = await fetch("https://muzair-010.huggingface.space/upload", {
+      const res = await fetch("https://muzair-010.huggingface.space/analyze-call", {
         method: "POST",
         body: formData,
       });
